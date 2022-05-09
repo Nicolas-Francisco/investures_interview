ebcec423e2a3ae54465512d0cc8a0654a39fcc94
Nicolás García <nicolas.garcia.r@ug.uchile.cl>
2022-05-09 12:27:30 -0400

    main page almost finished, pill-card component created

---
+++ src/app/pill-card/pill-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pill-card',
+  templateUrl: './pill-card.component.html',
+  styleUrls: ['./pill-card.component.scss']
+})
+export class PillCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
